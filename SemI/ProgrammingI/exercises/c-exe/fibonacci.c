7bcf49b731cab15afe0db6283450a19b110ee8f0
gabrielfelipe <gabrielfelipeassuncaodesouza@gmail.com>
2023-07-17 14:37:56 -0300

    add fibonnaci exercise

---
+++ SemI/ProgrammingI/exercises/c-exe/fibonacci.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+	int n1=0, n2=1;
+	int lim;
+	printf("Digite quantos números da sequencia quer exibir: ");
+	scanf("%d", &lim);
+
+	for(int i = 0; i < lim; i++) {
+		if(i%2 == 0) {
+			printf("%d ", n1);
+			continue;
+		}
+
+		printf("%d ", n2);
+		n1+=n2;
+		n2+=n1;
+	}
+}
