cdeee2e7150f8ae8713bb35729359449038d82a0
gabrielfelipeassuncaodesouza <gabrielfelipeassuncaodesouza@gmail.com>
2023-08-28 10:12:48 -0300

    add express module

--- complementar_courses/expressjs/index.js
+++ complementar_courses/expressjs/index.js
@@ -1,1 +1,10 @@
-console.log("Hello, World!")
+const express = require('express')
+const app = express()
+
+app.get('/ola', (request, response) => {
+    response.json({Hello: "World"})
+})
+
+app.listen(3030, v => {
+    console.log("Server running on port 3030");
+})
