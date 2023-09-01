dbb04ece8ca650de215670b36bde8c4131d1fb64
gabrielfelipeassuncaodesouza <gabrielfelipeassuncaodesouza@gmail.com>
2023-09-01 10:30:54 -0300

    add aula02 of expressjs course

---
+++ complementar_courses/expressjs/aula02/main.js
@@ -0,0 +1,6 @@
+const ops = require('./modules.js')
+
+console.log(ops.soma(1, 3))
+console.log(ops.sub(1, 3))
+console.log(ops.multi(1, 3))
+console.log(ops.div(1, 3))
---
+++ complementar_courses/expressjs/aula02/modules.js
@@ -0,0 +1,17 @@
+function soma(a, b) {
+    return a + b
+}
+
+function sub(a, b) {
+    return a - b
+}
+
+function multi(a, b) {
+    return a * b
+}
+
+function div(a, b) {
+    return a / b
+}
+
+module.exports = {soma, sub, multi, div}
