1fbb4ceb65a02f25db90e029bb08351f9e332d04
gabrielfelipe <gabrielfelipeassuncaodesouza@gmail.com>
2023-09-06 00:13:32 -0300

    move battleship game of its own repository

--- semI/programmingI/battleship/globconst.h
+++
@@ -1,6 +0,0 @@
-#ifndef _H_CONSTS
-#define _H_CONSTS
-
-#define SHIPS 5
-
-#endif
--- semI/programmingI/battleship/shoot.h
+++
@@ -1,8 +0,0 @@
-#ifndef _H_SHOOT
-#define _H_SHOOT
-
-#include "board.h"
-
-int shoot(char board[][TAM]);
-
-#endif
