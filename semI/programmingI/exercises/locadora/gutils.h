0f4bfd6a2e65f3138d30040d51c9d3d8f8c616f9
gabrielfelipe <gabrielfelipeassuncaodesouza@gmail.com>
2023-09-08 15:21:49 -0300

    remove locadora from this repo and put it in its own repo

--- semI/programmingI/exercises/locadora/gutils.h
+++
@@ -1,8 +0,0 @@
-#ifndef _H_GUTILS
-#define _H_GUTILS
-
-void readInt(const char* msg, int* dest);
-void readFloat(const char* msg, float* dest);
-char* readLine(const char* msg);
-
-#endif // !_H_GUTILS
