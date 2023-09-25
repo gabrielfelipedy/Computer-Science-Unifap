c20aca23a81ae1f1d53b43eec0d4406d11e8b6e2
gabrielfelipeassuncaodesouza <gabrielfelipeassuncaodesouza@gmail.com>
2023-09-25 10:14:26 -0300

    add first exercise of promisses

---
+++ complementar_courses/expressjs/exercises/promisses/ex01.js
@@ -0,0 +1,37 @@
+let people = [
+  {
+    name: "Gabriel",
+    age: 18
+  },
+  {
+    name: "Lucas",
+    age: 15
+  },
+  {
+    name: "Kelvin",
+    age: 30
+  }
+]
+
+function process(people) {
+  return new Promise((resolve, reject) => {
+    if(!people) {
+      reject("Error, empty or non avaliable people")
+    }
+
+    let legalAges = 0
+    people.forEach(person => {
+      if(person.age >= 18) legalAges++
+    })
+
+    if(legalAges == 0) {
+      reject("No legal of ages in this list")
+    }
+
+    resolve(`There're ${legalAges} people legal of age`)
+  })
+  .then(result => console.log(result))
+  .catch(err => console.log(err))
+}
+
+process(people)
