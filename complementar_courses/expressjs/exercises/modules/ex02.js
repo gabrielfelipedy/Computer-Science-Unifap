04c2a1e5b9230634418969c595ced73ca5c68eab
gabrielfelipeassuncaodesouza <gabrielfelipeassuncaodesouza@gmail.com>
2023-09-25 10:03:18 -0300

    add exercise 02 of modules

---
+++ complementar_courses/expressjs/exercises/modules/ex02.js
@@ -0,0 +1,4 @@
+import { media } from "./modules.js"
+
+console.log(media([10, 4, 8]))
+console.log(media([8, 5]))
--- complementar_courses/expressjs/exercises/modules/modules.js
+++ complementar_courses/expressjs/exercises/modules/modules.js
@@ -1,3 +1,15 @@
 export function sum(a, b) {
   return a + b
-} 
+}
+
+export function media(notes) {
+  let media = 0
+  notes.forEach(note => {
+    media+=note
+  });
+
+  media /= notes.length
+
+  if(media >= 7) return 'Aprovado'
+  else return 'Reprovado'
+}
