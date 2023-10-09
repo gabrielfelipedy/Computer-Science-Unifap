71fba60f2436d48fde1e9c7a6ebe1b95c8a08861
gabrielfelipeassuncaodesouza <gabrielfelipeassuncaodesouza@gmail.com>
2023-10-09 14:45:54 -0300

    remove unecessary files

--- semI/programmingI/exercises/desafios/pascal.c
+++ semI/programmingI/exercises/desafios/pascal.c
@@ -4,14 +4,14 @@
 * This function fills the triangle with the correct values
 */
 
-void fill_trian(int alt, int trian[alt][alt]) {
+void fill_trian(int alt, int trian[][alt]) {
   for(int i = 0; i < alt; i++) {
     for(int j = 0; j <= i; j++) {
-      if(j == 0 || j == i) {
+
+      if(j == 0 || j == i)
         trian[i][j] = 1;
-        continue;
-      }
-      trian[i][j] = trian[i-1][j-1] + trian[i-1][j];
+      else
+        trian[i][j] = trian[i-1][j-1] + trian[i-1][j];
     }
   }
 }
@@ -22,7 +22,9 @@ void fill_trian(int alt, int trian[alt][alt]) {
 
 void print_trian(int alt, int trian[alt][alt]) {
  for(int i = 0; i < alt; i++) {
+
     for(int k = alt-i; k >= 0; k--) putchar(' ');
+
     for(int j = 0; j <= i; j++) {
       printf("%d ", trian[i][j]);
     }
@@ -34,7 +36,9 @@ int main() {
   int alt;
   printf("Digite a altura do triângulo de pascal: ");
   scanf("%d", &alt);
+
   int trian[alt][alt];
+
   fill_trian(alt, trian);
   print_trian(alt, trian);
  }
